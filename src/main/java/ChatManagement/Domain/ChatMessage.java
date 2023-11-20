eb2c6965b6a3e32f5116e5a24e5f279179d88c9a
SangHee Lee <lsh277604@gmail.com>
2023-11-21 02:59:08 +0900

    Refactor: delete anotation

--- src/main/java/ChatManagement/Domain/ChatMessage.java
+++ src/main/java/ChatManagement/Domain/ChatMessage.java
@@ -1,2 +1,18 @@
-package ChatManagement.Domain;public class ChatMessage {
+package ChatManagement.Domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ChatMessage  {
+    private Long roomId;
+    private Long senderId;
+    private Long receiverId;
+    private String message;
 }
