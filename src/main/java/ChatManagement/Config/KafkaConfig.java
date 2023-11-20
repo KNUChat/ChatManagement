772752a2c97a32893ffd35eb4fdcb4472c3bbf42
SangHee Lee <lsh277604@gmail.com>
2023-11-21 02:58:40 +0900

    Feat: add TrustPackage and Class Mapping

--- src/main/java/ChatManagement/Config/KafkaConfig.java
+++ src/main/java/ChatManagement/Config/KafkaConfig.java
@@ -1,15 +1,17 @@
 package ChatManagement.Config;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import ChatManagement.Domain.ChatMessage;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @Configuration
 @PropertySource("classpath:application-kafka.properties")
@@ -17,18 +19,31 @@ public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
 
-    @Value("${spring.kafka.producer.topic}")
-    private String producerDefaultTopic;
+    @Value("${spring.kafka.consumer.topic}")
+    private String consumerDefaultTopic;
 
-    public ConsumerFactory<Object, Object> consumerFactory(){
-        Map<String,Object> props = new HashMap<>();
+    @Value("${spring.kafka.consumer.group-id}")
+    private String consumerDefaultGroupId;
+
+    @Bean
+    public ConsumerFactory<Object, Object> consumerFactory() {
+
+        JsonDeserializer<Object> deserializer = new JsonDeserializer<>(Object.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+
+        Map<String, Object> props = new HashMap<>();
 
         //Customized
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerDefaultGroupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserialize.class);
-
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*"); // 이부분
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, "false");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, ChatMessage.class);
         return new DefaultKafkaConsumerFactory<>(props);
     }
+
 }
