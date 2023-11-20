8885402c129847a09ba688453f79442f0f791f9a
SangHee Lee <lsh277604@gmail.com>
2023-11-21 01:43:24 +0900

    Initial Commit

---
+++ src/test/java/ChatManagement/ChatManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ChatManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
