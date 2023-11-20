e93d8466215e4b57698057cbf2743136c38b9068
SangHee Lee <lsh277604@gmail.com>
2023-11-21 03:05:24 +0900

    Feat: kafka connect with another service

--- src/main/java/ChatManagement/Service/KafkaService.java
+++ src/main/java/ChatManagement/Service/KafkaService.java
@@ -1,2 +1,17 @@
-package ChatManagement.Service;public class KafkaService {
+package ChatManagement.Service;
+
+import ChatManagement.Domain.ChatMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaService {
+
+    @KafkaListener(topics = "chatlog")
+    public void test( ChatMessage chatMessage){
+        log.info(String.valueOf(chatMessage));
+    }
 }
