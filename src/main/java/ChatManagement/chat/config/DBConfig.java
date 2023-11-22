9bf2d2e3018a9b7b749238fa8820527f45daea61
SangHee Lee <lsh277604@gmail.com>
2023-11-22 11:06:04 +0900

    fix: activate database dependency

---
+++ src/main/java/ChatManagement/chat/config/DBConfig.java
@@ -0,0 +1,9 @@
+package ChatManagement.chat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class config {
+}
---
+++ src/main/java/ChatManagement/chat/dao/RoomStatus.java
@@ -0,0 +1,2 @@
+package ChatManagement.chat.dao;public record RoomStatus() {
+}
