5d976d78ed92eb48fc8acc585c02e666d3caf9c9
Sanghee0820 <lsh277604@gmail.com>
2023-11-23 10:42:02 +0900

    feat: add findByMentorIdAndRoomStatus

--- src/main/java/ChatManagement/chat/repository/ChatMessageRepository.java
+++ src/main/java/ChatManagement/chat/repository/ChatMessageRepository.java
@@ -1,7 +1,10 @@
 package ChatManagement.chat.repository;
 
 import ChatManagement.chat.dao.ChatMessage;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    List<ChatMessage> findChatMessagesByRoomId(Long roomId);
 }
