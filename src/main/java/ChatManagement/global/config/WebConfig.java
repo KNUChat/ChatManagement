dc16188bcffcaacad1ad885221a3318cd436bfbe
Sanghee0820 <lsh277604@gmail.com>
2023-12-03 18:44:29 +0900

    refactor: WebConfig 패키지 추가

--- src/main/java/ChatManagement/global/config/WebConfig.java
+++ src/main/java/ChatManagement/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package ChatManagement.global;
+package ChatManagement.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
