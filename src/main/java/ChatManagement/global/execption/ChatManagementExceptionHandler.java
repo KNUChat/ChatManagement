7a129e17f58901ab99fc27454ab9e2bb02f356c1
Sanghee0820 <lsh277604@gmail.com>
2023-12-03 19:22:54 +0900

    feat: add 채팅룸 생성 에러 핸들러

--- src/main/java/ChatManagement/global/execption/ChatManagementExceptionHandler.java
+++ src/main/java/ChatManagement/global/execption/ChatManagementExceptionHandler.java
@@ -1,7 +1,9 @@
 package ChatManagement.global.execption;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,6 +19,16 @@ public class ChatManagementExceptionHandler {
                 .status(e.getHttpStatus())
                 .body(new ErrorResponse(
                         e.getHttpStatus().value(),
+                        e.getMessage()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> requestParamException(HttpMessageNotReadableException e){
+        log.info("Exception check  : " + e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(
+                        HttpStatus.BAD_REQUEST.value(),
                         e.getMessage()));
     }
 }
