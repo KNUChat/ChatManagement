607da9678d3f09487a2003939786121a0c47eb5a
Sanghee0820 <lsh277604@gmail.com>
2023-12-10 17:31:28 +0900

    fix: CORS[POST]추가

--- src/main/java/ChatManagement/global/config/WebConfig.java
+++ src/main/java/ChatManagement/global/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*") // 모든 origin 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true)
                 .allowedHeaders("*");
     }
