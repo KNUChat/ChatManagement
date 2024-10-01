b5d56653ccb142d08b50e7453e1194763ecfc7a1
sanghee0820 <lsh277604@gmail.com>
2024-10-02 00:34:19 +0900

    refactor: domain 변경에 따른 매서드 추가 및 삭제

--- src/main/java/ChatManagement/chat/persistence/ChatMessageRepository.java
+++ src/main/java/ChatManagement/chat/persistence/ChatMessageRepository.java
@@ -1,10 +1,11 @@
 package ChatManagement.chat.persistence;
 
 import ChatManagement.chat.domain.Message;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<Message, Long> {
-
+    List<Message> findByRoomId(Long roomId);
 }
