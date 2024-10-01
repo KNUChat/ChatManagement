d00cd58ad54cbc9fcb98d6fccf7e331998f8d869
sanghee0820 <lsh277604@gmail.com>
2024-10-02 00:33:39 +0900

    refactor: dto를 command, Info로 변경 후 팩토리 메서드 사용

--- src/main/java/ChatManagement/chat/presentation/dto/ChatMessageResponse.java
+++ src/main/java/ChatManagement/chat/presentation/dto/ChatMessageResponse.java
@@ -1,32 +1,23 @@
 package ChatManagement.chat.presentation.dto;
 
-
-import ChatManagement.chat.domain.Message;
+import ChatManagement.chat.application.dto.MessageInfo;
 import ChatManagement.chat.domain.status.ChatMessageType;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder @Getter
-public class ChatMessageResponse {
-
-    private Long senderId;
-    private Long receiverId;
-    private String message;
-    private ChatMessageType chatMessageType;
-    private LocalDateTime sendTime;
-
-    public static ChatMessageResponse from(Message message){
-        return ChatMessageResponse.builder()
-                .message(message.getMessage())
-                .senderId(message.getSenderId())
-                .receiverId(message.getReceiverId())
-                .chatMessageType(message.getChatMessageType())
-                .sendTime(message.getSendTime())
-                .build();
+public record ChatMessageResponse(
+        Long senderId,
+        Long receiverId,
+        String message,
+        ChatMessageType chatMessageType,
+        LocalDateTime sendTime
+) {
+    public static ChatMessageResponse from(MessageInfo message) {
+        return new ChatMessageResponse(
+                message.senderId(),
+                message.receiverId(),
+                message.message(),
+                message.chatMessageType(),
+                message.sendTime()
+        );
     }
 }
