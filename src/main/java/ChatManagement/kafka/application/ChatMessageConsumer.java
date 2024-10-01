ad23517556de0d6e21d7968026509a1f8986afc7
sanghee0820 <lsh277604@gmail.com>
2024-10-02 00:48:35 +0900

    refactor: 메시지 Consumer 클래스 이름 변경

--- src/main/java/ChatManagement/kafka/application/ChatMessageConsumer.java
+++ src/main/java/ChatManagement/kafka/application/ChatMessageConsumer.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class KafkaConsumer {
+public class ChatMessageConsumer {
     private final ChatRoomService chatRoomService;
 
     @KafkaListener(topics = "chatlog", errorHandler = "kafkaListenerErrorHandler")
